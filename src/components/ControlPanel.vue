099d3ff59bbb051986d5bc357a790d2079cdbe8d
tch1001 <tanchienhao@gmail.com>
2022-08-27 17:38:11 +0800

    implemented adding of edges

---
+++ src/components/ControlPanel.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="control-panel">
+        <button @click="this.$emit('home')">Home</button>
+        <select v-model="selected_mode" class="flush-right">
+            <option value="add-node">Add Vertex (V)</option>
+            <option value="add-edge">Add Edge (E)</option>
+            <option value="delete-node">Delete Vertex (D)</option>
+            <option value="move">Look Around (A)</option>
+        </select>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "ControlPanel",
+    data(){
+        return {
+            selected_mode: 'add-edge'
+        }
+    },
+    methods:{
+        
+    },
+    mounted(){
+
+    }
+})
+</script>
+<style scoped>
+.control-panel{
+    width: 60%;
+    background-color: lightblue;
+}
+.flush-right{
+    right: inherit;
+}
+</style>
--- src/views/Graph.vue
+++ src/views/Graph.vue
@@ -1,35 +1,59 @@
 <template>
     <div id="graph">
-        <v-network-graph ref="graphRef" v-model:selected-nodes="graphData.selectedNodes"
+        <v-network-graph ref="graph_ref" v-model:selected-nodes="graphData.selectedNodes"
             v-model:selected-edges="graphData.selectedEdges" :nodes="graphData.nodes"
             :edges="graphData.edges" :layouts="graphData.layouts"
             :configs="graphData.configs" :event-handlers="eventHandlers" />
     </div>
     <DataPanelVue :data_panel="dataPanel"></DataPanelVue>
+    <ControlPanelVue @home="home" ref="control_panel_ref"></ControlPanelVue>
 </template>
 <script lang="ts">
 import axios from "axios";
 import DataPanelVue from "../components/DataPanel.vue";
+import ControlPanelVue from "../components/ControlPanel.vue";
 import { defineComponent, reactive, ref } from "vue";
 import graphData from "../graphData"
-import { EventHandlers } from "v-network-graph";
 var dataPanel: any = ref({});
 
 const project_id: any = ref("");
-const graphRef: any = ref();
+const graph_ref: any = ref();
+const control_panel_ref: any = ref();
 
 function add_node(raw: any) {
     graphData.nodes.value[`node${raw.uid}`] = raw
 }
-
-function add_edge(raw: any) {
-    graphData.edges.value[raw[1].uid] =
+function add_edge(s: string, t: string, uid: any){
+    graphData.edges.value[uid] =
     {
-        source: `node${raw[0]}`,
-        target: `node${raw[2]}`,
-        uid: raw[1].uid
+        source: s,
+        target: t,
+        uid: uid
     }
 }
+var edge_source_node : any = null;
+function create_edge(s: string, t: string){
+    console.log(s.substring(4))
+    console.log(t.substring(4))
+    const request_url = `${import.meta.env.VITE_API_URL}`+
+                    `/project/${project_id.value}`+
+                    `/new?item=relationship`+
+                    `&a_id=${s.substring(4)}&b_id=${t.substring(4)}`
+    axios.post(request_url)
+        .then(response => {
+            graphData.edges.value[response.data.uid] = {
+                source: s,
+                target: t,
+                uid: response.data.uid
+            }
+        })
+    edge_source_node = null
+}
+
+function add_edge_raw(raw: any) {
+    add_edge(`node${raw[0]}`, `node${raw[2]}`, raw[1].uid)
+}
+
 function get_items() {
     const request_url = import.meta.env.VITE_API_URL + `/project/${project_id.value}`
     axios
@@ -49,18 +73,19 @@ function get_items() {
                 }
             }
             for (const item of edge_queue) {
-                add_edge(item)
+                add_edge_raw(item)
             }
         })
 }
 export default defineComponent({
     name: "Graph",
     components: {
-        DataPanelVue
+        DataPanelVue,
+        ControlPanelVue
     },
     data() {
         return {
-            graphRef,
+            graph_ref,
             project_id,
             graphData,
             dataPanel,
@@ -71,35 +96,78 @@ export default defineComponent({
                     if (type == 'node:pointerover') {
                         dataPanel.value = graphData.nodes.value[event.node]
                     } else if (type == 'view:click') {
-                        const request_url = `${import.meta.env.VITE_API_URL}` +
-                            `/project/${project_id.value}` +
-                            `/new?item=node`
-                        axios
-                            .post(request_url)
-                            .then(response => {
-                                this.add_node_with_mouse(response.data, event.event)
-                            })
+                        this.handle_view_click(event)
+                    } else if (type == 'node:select'){
+                        this.handle_node_select(event)
+                    } else if(type == 'node:click'){
+                        this.handle_node_click(event)
                     }
                 },
             }
         }
     },
     methods: {
-        onInputPropertyName: function () {
-
+        handle_view_click: function (event: any) {
+            const selected_mode = this.$refs.control_panel_ref.selected_mode 
+            if (selected_mode == 'add-node') {
+                const request_url = `${import.meta.env.VITE_API_URL}` +
+                    `/project/${project_id.value}` +
+                    `/new?item=node`
+                axios
+                    .post(request_url)
+                    .then(response => {
+                        this.add_node_with_mouse(response.data, event.event)
+                    })
+                this.$refs.control_panel_ref.selected_mode = 'move'
+            }else if(selected_mode == 'add-edge'){
+                edge_source_node = null;
+            }
         },
-        onInputPropertyValue: function () {
-
+        handle_node_click: function(event: any){
+            const selected_mode = this.$refs.control_panel_ref.selected_mode 
+            if(selected_mode == 'add-edge'){
+                if(edge_source_node){
+                    create_edge(edge_source_node, event.node)
+                    this.$refs.control_panel_ref.selected_mode = 'move'
+                }
+            }
         },
-        onInputLabel: function () {
-
+        handle_node_select: function(event: any){
+            const selected_mode = this.$refs.control_panel_ref.selected_mode
+            if(selected_mode == 'add-edge'){
+                if(event.length == 0) edge_source_node = null;
+                else if(event.length == 1){
+                    edge_source_node = event[0]
+                }else if(event.length == 2){
+                    create_edge(event[0], event[1])
+                    this.$refs.control_panel_ref.selected_mode = 'move'
+                } 
+            }
+        },
+        home: function () {
+            const inf = 10000000000000000;
+            var minX = inf, minY = inf, maxX = -inf, maxY = -inf;
+            for (const i in graphData.layouts.value.nodes) {
+                const node = graphData.layouts.value.nodes[i]
+                minX = Math.min(minX, node.x)
+                maxX = Math.max(maxX, node.x)
+                minY = Math.min(minY, node.y)
+                maxY = Math.max(maxY, node.y)
+            }
+            const padding = 100;
+            this.$refs.graph_ref.setViewBox({
+                left: minX - padding,
+                top: minY - padding,
+                right: maxX + padding * 5,
+                bottom: maxY + padding,
+            })
         },
         get_items,
         // add_node,
-        // add_edge,
-        add_node_with_mouse: function add_node_with_mouse(raw: any, e : any){
-            const point = {x: e.offsetX, y: e.offsetY}
-            const svgPoint = this.$refs.graphRef.translateFromDomToSvgCoordinates(point)
+        // add_edge_raw,
+        add_node_with_mouse: function (raw: any, e: any) {
+            const point = { x: e.offsetX, y: e.offsetY }
+            const svgPoint = this.$refs.graph_ref.translateFromDomToSvgCoordinates(point)
             graphData.layouts.value.nodes[`node${raw.uid}`] = svgPoint
             add_node(raw)
         }
