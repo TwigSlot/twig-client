22514b88669366c6f47ebca603bbe4b8fd2d9b90
tch1001 <tanchienhao@gmail.com>
2022-10-05 13:16:49 +0800

    added dissociating of tags!

--- src/components/DecoPanel.vue
+++ src/components/DecoPanel.vue
@@ -6,7 +6,9 @@
                     Tags: {{ showing_tags_for }}
                 </h1>
                 <div id="tags-list">
-                    <text v-for="tag in tags_list" class="subtitle tag">{{(tag as any).name}}</text>
+                    <text @contextmenu.prevent="dissociate_tag((tag as any).name)" v-for="tag in tags_list" class="subtitle tag">
+                        {{(tag as any).name}}
+                    </text>
                 </div>
                 <br>
                 <input class="input is-hovered info-panel-item" type="text" placeholder="Color"
@@ -17,8 +19,10 @@
                     <option v-for="tag in tags_suggestions_list" :value="(tag as any).name">{{`uid: (${(tag as
                     any).uid})`}}</option>
                 </datalist>
-                <input class="button is-primary" type="button" value="Add Tag" @click="add_tag" :disabled="disable_add"/>
-                <input class="button is-primary" type="button" value="List ALL Tags in Project" @click="list_all_tags(true)" />
+                <input class="button is-primary" type="button" value="Add Tag" @click="add_tag"
+                    :disabled="disable_add" />
+                <input class="button is-primary" type="button" value="List ALL Tags in Project"
+                    @click="list_all_tags(true)" />
                 <input class="button is-primary" type="button" value="Delete Tag (by uid)" @click="delete_tag" />
             </div>
         </div>
@@ -96,7 +100,6 @@ export default defineComponent({
             var first_tag_matching_name = this.tag_exists(tags, tag_name.value)
             console.log(first_tag_matching_name)
             if (first_tag_matching_name == null) {
-                console.log('creating')
                 first_tag_matching_name = await this.create_tag().then((res) => { return res; })
             }
             const resource_id = this.$props.data_panel.uid;
@@ -168,21 +171,24 @@ export default defineComponent({
                 })
         },
         dissociate_tag: async function (name_of_tag: string) {
+            console.log('diss', name_of_tag)
             var first_tag_matching_name = this.tag_exists(tags_list.value, name_of_tag)
-            if (first_tag_matching_name == null) {
-                console.log('creating')
-                first_tag_matching_name = await this.create_tag().then((res) => { return res; })
-            }
+            if (first_tag_matching_name == null) return
             const resource_id = this.$props.data_panel.uid;
             if (!resource_id) return
             const request_url = `${import.meta.env.VITE_API_URL}` +
                 `/project/${this.$props.project_id}` +
                 `/resource/${resource_id}` +
-                `/add_tag?tag_uid=${(first_tag_matching_name as any).uid}`
+                `/dissociate_tag?tag_uid=${(first_tag_matching_name as any).uid}`
             axios.post(request_url)
                 .then((response) => {
-                    (tags_list.value as any).push(response.data)
+                    console.log(response.data)
+                    this.list_tags()
                 })
+        },
+        filter_suggestions: function () {
+            tags_suggestions_list.value = tags_suggestions_list.value.filter((v: any) =>
+                (this.tag_exists(tags_list.value, v.name) == null))
         }
     },
     data() {
@@ -198,8 +204,7 @@ export default defineComponent({
         async 'data_panel.uid'(new_value) {
             await this.list_tags()
             await this.list_all_tags(false)
-            tags_suggestions_list.value = tags_suggestions_list.value.filter((v: any) => 
-                            (this.tag_exists(tags_list.value, v.name) == null))
+            this.filter_suggestions()
             showing_tags_for.value = "Node " + new_value
         }
     }
