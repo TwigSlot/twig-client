d7daa656bbc0947a12beb4512d66454c7aa7a926
tch1001 <tanchienhao@gmail.com>
2022-10-05 09:13:59 +0800

    basic tagging

--- src/components/DecoPanel.vue
+++ src/components/DecoPanel.vue
@@ -6,14 +6,19 @@
                     Tags:
                 </h1>
                 <div id="tags-list">
-                    <text class="subtitle tag">hi</text>
+                    <text v-for="tag in tags_list" class="subtitle tag">{{(tag as any).name}}</text>
                 </div>
+                <br>
                 <input class="input is-hovered info-panel-item" type="text" placeholder="Color"
                     v-model="data_panel.color" @focus="pauseKeyDown" @blur="handleBlur()" />
-                <input class="input is-hovered info-panel-item" type="text" placeholder="Tag" v-model="tag_name" />
-                <input class="button is-primary" placeholder="Add Tag" @click="add_tag" />
-                <input class="button is-primary" placeholder="List Tags" @click="list_tags" />
-                <input class="button is-primary" placeholder="Delete Tag (by uid)" @click="delete_tag" />
+                <input class="input is-hovered info-panel-item" type="text" placeholder="Tag" v-model="tag_name" 
+                        autocomplete="on" list="autocomplete_tags"/>
+                <datalist id="autocomplete_tags">
+                    <option v-for="tag in tags_suggestions_list" :value="(tag as any).name">{{`uid: (${(tag as any).uid})`}}</option>
+                </datalist>
+                <input class="button is-primary" type="button" value="Add Tag" @click="add_tag" />
+                <input class="button is-primary" type="button" value="List ALL Tags" @click="list_all_tags" />
+                <input class="button is-primary" type="button" value="Delete Tag (by uid)" @click="delete_tag" />
             </div>
         </div>
         <div id="deco-box">
@@ -63,6 +68,8 @@ import { ta } from "element-plus/es/locale";
 import { defineComponent, ref } from "vue";
 
 const tag_name = ref("")
+const tags_list = ref([]);
+const tags_suggestions_list = ref([]);
 export default defineComponent({
     name: "DecoPanel",
     props: ["data_panel", "project_id"],
@@ -73,43 +80,77 @@ export default defineComponent({
         pauseKeyDown: function () {
             this.$emit("pauseKeyDown");
         },
-        add_tag: function () {
+        // checks whether name_of_tag is inside tags
+        tag_exists: function(tags: any, name_of_tag: string) {
+            const tags_matching_name = tags.filter((tag : any) => tag.name == name_of_tag)
+            if(tags_matching_name.length == 0) return tags_matching_name[0]
+            else return null
+        },
+        add_tag: async function (){
+            if(tag_name.value == "") return
+            const tags = await this.list_all_tags(false)
+            var first_tag_matching_name = this.tag_exists(tags, tag_name.value)
+            if(first_tag_matching_name == null){
+                first_tag_matching_name = await this.create_tag().then((res) => {return res;})
+            }
+            const resource_id = this.$props.data_panel.uid;
+            if(!resource_id) return
+            const request_url = `${import.meta.env.VITE_API_URL}` +
+                `/project/${this.$props.project_id}` +
+                `/resource/${resource_id}` +
+                `/add_tag?tag_uid=${(first_tag_matching_name as any).uid}`
+            axios.post(request_url)
+                .then((response) => {
+                    (tags_list.value as any).push(response.data)
+                })
+        },
+        create_tag: async function () {
             if (tag_name.value == "") return
-            console.log("adding", tag_name.value)
-            console.log('inside project', this.$props.project_id)
             const request_url = `${import.meta.env.VITE_API_URL}` +
                 `/project/${this.$props.project_id}` +
                 `/create_tag?name=${tag_name.value}`
-            axios.post(request_url)
+            return axios.post(request_url)
                 .then((response) => {
-                    console.log(response.data)
+                    return response.data
                 })
         },
-        list_tags: async function () {
+        list_tags: async function (){
+            const resource_id = this.$props.data_panel.uid;
+            if(!resource_id) return
             const request_url = `${import.meta.env.VITE_API_URL}` +
                 `/project/${this.$props.project_id}` +
+                `/resource/${resource_id}` +
                 `/list_tags`
             return axios.get(request_url)
                 .then((response) => {
-                    console.log(response.data)
+                    tags_list.value = response.data
+                    return response.data
+                })
+        },
+        list_all_tags: async function (update_tags_list = true) {
+            const request_url = `${import.meta.env.VITE_API_URL}` +
+                `/project/${this.$props.project_id}` +
+                `/list_all_tags`
+            return axios.get(request_url)
+                .then((response) => {
+                    if(update_tags_list) tags_list.value = response.data
+                    tags_suggestions_list.value = response.data
                     return response.data
                 })
         },
         delete_tag: async function () {
-            const tags = await this.list_tags().then((arr) => {return arr;})
-            console.log(tags)
+            const tags = await this.list_all_tags().then((arr) => {return arr;})
             try{
                 assert(!isNaN(parseInt(tag_name.value)))
             }catch{
                 return
             }
-            if(tags.filter((tag : any) => tag.uid == parseInt(tag_name.value)).length == 0) return
+            if(tags.filter((tag : any) => tag.uid == parseInt(tag_name.value)).length == 0) return 
             const request_url = `${import.meta.env.VITE_API_URL}` +
                 `/project/${this.$props.project_id}` +
                 `/delete_tag?uid=${tag_name.value}`
             axios.post(request_url)
                 .then((response) => {
-                    console.log(response.data)
                 })
                 .catch((err) => {
                     console.log(err)
@@ -119,7 +160,14 @@ export default defineComponent({
     },
     data() {
         return {
-            tag_name
+            tag_name,
+            tags_list,
+            tags_suggestions_list
+        }
+    },
+    watch: {
+        'data_panel.uid'(new_value){
+            this.list_tags()
         }
     }
 });
