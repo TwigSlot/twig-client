2b29687952f7775e8d5ca4d4b3ee443e9511c2b0
Robert <xiurobert@gmail.com>
2022-10-16 16:32:08 +0800

    Remove package-lock, we use yarn

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0"
+  },
   plugins: [vue()],
   css: {
     preprocessorOptions: {
